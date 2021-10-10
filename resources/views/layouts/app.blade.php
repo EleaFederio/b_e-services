ac8d049b68aac0a8216a872b4fa8e3d106336138
Eleazar O. Federio <eleazarfederio@rocketmail.com>
2021-10-10 15:53:19 +0800

    medyo pwede na

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
     <style>
         body{
             background: #599fd9;
-            background-image: linear-gradient(135.9deg, rgba(109,25,252,1)  16.4%, rgba(125,31,165,1)  56.1%);
+            background-image: linear-gradient(135.9deg, rgba(255, 244, 224,1)  16.4%, rgba(224, 233, 255 ,1)  56.1%);
             min-height: 100vh;
             overflow-x: hidden;
         }
@@ -32,6 +32,10 @@
             box-shadow: 3px 3px 10px   rgba(0, 0,0,0.1);
             transition: all  0.4s;
         }
+        .bg-dark{
+            background-color: #3e6196 !important;
+            color: white;
+        }
         .page-content{
             width: calc(100% - 17rem);
             margin-left: 17rem;
@@ -40,7 +44,7 @@
         #sidebar.active{
             margin-left: -17rem;
         }
-        #content-active{
+        #content.active{
             width: 100%;
             margin: 0;
         }
@@ -61,7 +65,7 @@
             background: #f3f2f2;
         }
         .text-primary{
-            color: #7579e7 !important;
+            color: #000 !important;
         }
         @media (max-width: 768px) {
             #sidebar{
@@ -82,61 +86,6 @@
     </style>
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="{{ url('/') }}">
-{{--                    {{ config('app.name') }}--}}
-                    Barangay E-Services
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
 
         @auth()
             @include('partials.sidebar')
@@ -146,7 +95,7 @@
             @yield('content')
         </main>
     </div>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+{{--    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>--}}
     <script src="{{ asset('js/app.js') }}" defer></script>
 </body>
 </html>
--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,10 +1,9 @@
 <!-- Vertical Navbar -->
 <div class="vertical-nav  bg-white" id="sidebar">
 
-    <div class="py-4 px-3  mb-4 bg-light">
-        <div class="media d-flex  align-items-md-center">
-            <img src="{{ url('/images/spider.jpg') }}" height="80" width="80" class="mr-3 rounded-circle img-thumbnail shadow-sm">
-        </div>
+    <div class="py-4 px-3  mb-4 bg-light text-center">
+
+        <img src="{{ url('/images/spider.jpg') }}" height="80" width="80" class="mr-3 rounded-circle img-thumbnail shadow-sm">
         <div class="media-body">
             <h4 class="m-0">Juan de la  Cruz</h4>
             <p  class="font-weight-normal text-muted mb-0">Barangay Secretary</p>
@@ -28,52 +27,71 @@
         </li>
         <li class="nav-item">
             <a href="#" class="nav-link text-dark bg-light">
-                <i class="fa fa-cubes mr-3 text-primary fa-fw"></i>
+                <i class="fa fa-folder-open mr-3 text-primary fa-fw"></i>
                 Records
             </a>
         </li>
         <li class="nav-item">
             <a href="#" class="nav-link text-dark bg-light">
-                <i class="fa fa-cubes mr-3 text-primary fa-fw"></i>
+                <i class="fa fa-certificate mr-3 text-primary fa-fw"></i>
                 Certificates
             </a>
         </li>
         <li class="nav-item">
             <a href="#" class="nav-link text-dark bg-light">
-                <i class="fa fa-cubes mr-3 text-primary fa-fw"></i>
+                <i class="fa fa-briefcase mr-3 text-primary fa-fw"></i>
                 Officials
             </a>
         </li>
         <li class="nav-item">
             <a href="#" class="nav-link text-dark bg-light">
-                <i class="fa fa-cubes mr-3 text-primary fa-fw"></i>
+                <i class="fa fa-bullhorn mr-3 text-primary fa-fw"></i>
                 Report
             </a>
         </li>
+
     </ul>
 
-    <p class="text-gray font-weight-bold text-uppercase px-3 small py-4 mb-0">Charts</p>
+    <hr style="margin-bottom: auto">
 
-    <ul  class="nav flex-column bg-white mb-0">
-        <li class="nav-item">
-            <a href="#" class="nav-link text-dark bg-light">
-                <i class="fa fa-th-large mr-3 text-primary fa-fw"></i>
-                Home
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="#" class="nav-link text-dark bg-light">
-                <i class="fa fa-address-card mr-3 text-primary fa-fw"></i>
-                About
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="#" class="nav-link text-dark bg-light">
-                <i class="fa fa-cubes mr-3 text-primary fa-fw"></i>
-                Services
-            </a>
-        </li>
-    </ul>
+    <div class="container">
+        <footer>
+            <ul class="navbar-nav ml-auto">
+                <!-- Authentication Links -->
+                @guest
+                    @if (Route::has('login'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                    @endif
+
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                    @endif
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+            </ul>
+        </footer>
+    </div>
 
 </div>
 
@@ -81,40 +99,30 @@
 {{--Page  Content--}}
 <div class="page-content p-5" id="content">
 
+
     <!-- Toggle Button -->
-    <button id="sidebarCollapse" type="button" class="btn btn-light bg-white rounded-pill shadow-sm px-4 mb-4">
+    <button id="sidebarCollapse" onclick="change()" type="button" class="btn btn-light btn-sm bg-white rounded-pill shadow-sm px-4 mb-4">
         <i class="fa fa-bars mr-2"></i>
-        <small class="text-uppercase font-weight-bold">Toggle</small>
+        <small class="text-uppercase font-weight-bold"></small>
     </button>
 
-    <div class="container">
+    <div>
+        <div >
+
         <h1>Hello World</h1>
+        </div>
     </div>
 
 </div>
 
-@section('script')
+{{--@section('script')--}}
     <script type="text/javascript">
-        // $(function () {
-        //     $('#sidebarCollapse').on('click', function (){
-        //         alert('Yeaj')
-        //         $('#sidebar, #content').toggleClass('active')
-        //     });
-        // });
 
-
-        // $( "#sidebarCollapse" ).click(function() {
-        //     alert( "Handler for .click() called." );
-        // });
-
-        window.onload = function() {
-            if (window.jQuery) {
-                // jQuery is loaded
-                alert("Yeah!");
-            } else {
-                // jQuery is not loaded
-                alert("Doesn't Work");
-            }
+        function change(){
+            var element = document.getElementById('sidebar');
+            var element2 = document.getElementById('content');
+            element.classList.toggle("active");
+            element2.classList.toggle("active");
         }
     </script>
-@endsection
+{{--@endsection--}}
