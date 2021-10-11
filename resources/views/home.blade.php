59dd6afc790ee276d7fa8d6ec298d55b2147f54f
Eleazar O. Federio <eleazarfederio@rocketmail.com>
2021-10-11 23:28:41 +0800

    remove redundant render of home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,50 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container" style="background-color: red">
+<div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-{{--            <div class="card">--}}
-{{--                <div class="card-header">{{ __('Dashboard') }}</div>--}}
-
-{{--                <div class="card-body">--}}
-{{--                    @if (session('status'))--}}
-{{--                        <div class="alert alert-success" role="alert">--}}
-{{--                            {{ session('status') }}--}}
-{{--                        </div>--}}
-{{--                    @endif--}}
-
-{{--                    {{ __('You are logged in!') }}--}}
-{{--                </div>--}}
-{{--            </div>--}}
+        <div class="col-4">
+            <div class="card">
+                <div class="text-center">
+                    <img src="{{url('/images/icons/barangay_community.png')}}" width="150"  style="margin-top: 15px" alt="...">
+                </div>
+                <div class="card-body">
+                    <div class="text-center">
+                        <h3 class="card-title"><b>POPULATION</b></h3>
+                        <h1 class="display-text">1,200</h1>
+                    </div>
+                </div>
+            </div>
         </div>
+
+        <div class="col-4">
+            <div class="card">
+                <div class="text-center">
+                    <img src="{{url('/images/icons/barangay_blotter.png')}}" width="150"  style="margin-top: 15px" alt="...">
+                </div>
+                <div class="card-body">
+                    <div class="text-center">
+                        <h3 class="card-title"><b>BLOTTER</b></h3>
+                        <h1 class="display-text">10</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-4">
+            <div class="card">
+                <div class="text-center">
+                    <img src="{{url('/images/icons/barangay_household.png')}}" width="150"  style="margin-top: 15px" alt="...">
+                </div>
+                <div class="card-body">
+                    <div class="text-center">
+                        <h3 class="card-title"><b>HOUSEHOLD</b></h3>
+                        <h1 class="display-text">312</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </div>
 @endsection
