3d697227454fa7336efae262996b7fe6e48a47c4
Eleazar O. Federio <eleazarfederio@rocketmail.com>
2021-10-15 09:59:40 +0800

    rollback to migration only add update resident migration

--- database/migrations/2021_10_08_124019_create_residents_table.php
+++ database/migrations/2021_10_08_124019_create_residents_table.php
@@ -18,22 +18,24 @@ class CreateResidentsTable extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('middle_name');
-            $table->string('suffix');
+            $table->string('suffix')->nullable();
             $table->date('birth_date');
+            $table->string('phone_number');
+            $table->string('email');
             $table->string('birth_place');
 //            zone id
             $table->string('gender');
-            $table->string('skills');
+            $table->string('skills')->nullable();
 //            household id
-            $table->string('former_address');
-            $table->string('image_url');
+            $table->string('former_address')->nullable();
+            $table->string('image_url')->nullable();
 //            highest education attainment
-            $table->integer('length_of_stay'); // must be start  date
+            $table->integer('length_of_stay')->nullable(); // must be start  date
             $table->integer('occupation');
             $table->decimal('monthly_income', 9, 2);
 //            blood type must be relational
 //            marital status must be relational
-            $table->string('religion');
+            $table->string('religion')->nullable();
             $table->string('nationality'); // should use nationality api
             $table->timestamps();
         });
